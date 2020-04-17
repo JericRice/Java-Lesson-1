026ba8ea23105d5f1ee2533f2e9519a35c41d3d4
Joe <jeric720@icloud.com>
2020-04-16 20:31:56 -0400

    Initial commit

---
+++ src/myClass.java
@@ -0,0 +1,14 @@
+
+public class myClass {
+
+    public static void main(String[] args) {
+
+        // Variable == bucket in memory
+
+        
+
+
+        System.out.println("Hello World");
+        System.out.println("Another print line");
+    }
+}
